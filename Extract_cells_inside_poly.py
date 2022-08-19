96140e301f5b996ea38ca95148996c3e8d5c17ff
Nikolai Ponomarev <nponomar@daint103.login.cscs.ch>
2022-08-19 13:15:57 +0200

    Used xarray's isle method to extract cells. Worked well for a low res grid.

--- Extract_cells_inside_poly.py
+++ Extract_cells_inside_poly.py
@@ -41,19 +41,21 @@ fileN='/users/nponomar/grids/icon_Zurich_1_DOM02.nc'
 dataset = Dataset(fileN, maskandscale=True, mmap=False)
 dataset = xr.load_dataset(fileN)
 
-lats = dataset['vlat'][:]
-lons = dataset['vlon'][:]
+lats = dataset['clat'][:]
+lons = dataset['clon'][:]
 
 clon = np.rad2deg( lons )
 clat = np.rad2deg( lats )
 #print('Centers shape, lon, lat', np.shape(clon), np.shape(clat), clon, clat)
-print(dataset['vlat'] < dataset['vlat'].mean())
-dropped = dataset.where(dataset['vlat'] < dataset['vlat'].mean(), drop=True)
+
+#print(dataset['vlat'] < dataset['vlat'].mean())
+#dropped = dataset.where(dataset['vlat'] < dataset['vlat'].mean(), drop=True)
 #Search for indecies inside poly
-dropped.to_netcdf("/users/nponomar/grids/icon_Zurich_1_DOM02_droppedcells_python.nc" )
+#dropped.to_netcdf("/users/nponomar/grids/icon_Zurich_1_DOM02_droppedcells_python.nc" )
 
 #clon_poly = np.zeros(np.shape(clon), dtype=np.float64)
 #clat_poly = np.zeros(np.shape(clat), dtype=np.float64)
+
 indcs_poly=[]
 x = np.unique(clon)
 epsilon = (x[1] - x[0])
@@ -65,6 +67,7 @@ for ix in range(0, len(clon)):
 print('Centers inside poly shape, indecies', np.shape(indcs_poly), indcs_poly)
 
 #Extract cells 
+""""
 
 ifile = fileN
 ofile = "/users/nponomar/grids/icon_Zurich_1_DOM02_extractedcells_python.nc" 
@@ -74,6 +77,18 @@ inds = ','.join(str(e+1) for e in indcs_poly)
 cdo.selgridcell(inds, input=ifile, output=ofile)
 
 cdo.selgridcell()
+"""
+dataset_cells = dataset.isel(cell=indcs_poly) #extract vars which have cell dimension
+
+inds_verticies = np.array([[x for x in y] for y in dataset.vertex_of_cell[:, indcs_poly].values-1]).flatten()
+print('Extracted inds, inds_v', indcs_poly, inds_verticies)
+inds_edges = np.array([[x for x in y] for y in dataset.edge_of_cell[:, indcs_poly].values-1]).flatten()
+
+dataset_cells_verts = dataset_cells.isel(vertex=inds_verticies)
+dataset_cells_verts_edges = dataset_cells_verts.isel(edge=inds_edges)
+
+dataset_cells_verts_edges.to_netcdf("/users/nponomar/grids/icon_Zurich_1_DOM02_extracted_vars.nc" )
+
 #Plot the grid and city's shape file
 clon_poly = clon[indcs_poly]
 clat_poly = clat[indcs_poly]
@@ -85,16 +100,21 @@ plt.savefig('check.png')
 
 
 #Read newly created nc grid file
-fileN='/users/nponomar/grids/icon_Zurich_1_DOM02_extractedcells_python.nc'
+fileN='/users/nponomar/grids/icon_Zurich_1_DOM02_extracted_vars.nc'
 dataset = Dataset(fileN, maskandscale=True, mmap=False)
 dataset = xr.load_dataset(fileN)
 print(dataset)
+clats = dataset.variables['clat'][:]
+clons = dataset.variables['clon'][:]
 
-lats = dataset.variables['vlat'][:]
-lons = dataset.variables['vlon'][:]
+vlats = dataset.variables['vlat'][:]
+vlons = dataset.variables['vlon'][:]
 
-clon1 = np.rad2deg( lons )
-clat1 = np.rad2deg( lats )
+clon1 = np.rad2deg( clons )
+clat1 = np.rad2deg( clats )
+
+vlon1 = np.rad2deg( vlons )
+vlat1 = np.rad2deg( vlats )
 
 clon_poly = clon[indcs_poly]
 clat_poly = clat[indcs_poly]
@@ -104,5 +124,6 @@ ax.plot(clon, clat, 'o', color='black', markersize=2.5)
 ax.plot(clon_poly, clat_poly, 'o', color='green', markersize=2.5)
 ax.plot(np.asarray(coords)[:, 0], np.asarray(coords)[:, 1], 'o', color='black', markersize=2.5)
 ax.plot(clon1, clat1, 'o', color='red', markersize=2.5)
-plot_icongrid(dataset, ax)
-plt.savefig('Extracted_wit_cdo_points.png')
+ax.plot(vlon1, vlat1, 'o', color='gray', markersize=2.5)
+#plot_icongrid(dataset, ax)
+plt.savefig('Extracted_points_with_xr_isle.png')
